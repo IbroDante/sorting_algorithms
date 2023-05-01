cdd5a3f1cc5a0b154034c78ce48f619c1c0b0b3c
IbroDante <ibroman02@yahoo.com>
2023-05-01 16:14:59 +0000

    commit on sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,11 +7,11 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -23,20 +23,20 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+	size_t i, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap_ints(&array[j], &array[j + 1]);
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_ints(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
